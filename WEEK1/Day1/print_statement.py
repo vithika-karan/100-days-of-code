c22c19ecbc5796729a46fb2008bf97e69a9d8796
vithika-karan <vithika16k@gmail.com>
2023-04-28 22:39:11 +0530

    day 1

---
+++ WEEK1/Day1/print_statement.py
@@ -0,0 +1,9 @@
+'''Write a program that prints the use of the print function'''
+
+print('''Day 1 - Python Print Function''')
+print("The function is declared like this:")
+print("print('what to print')")
+
+# or
+
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
